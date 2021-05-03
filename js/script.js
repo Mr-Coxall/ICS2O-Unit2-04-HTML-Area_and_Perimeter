8866bd2c3abc8d4bf3ed42d2e81355d1d176d8df
Patrick Coxall <mr.coxall@mths.ca>
2021-05-03 02:02:07 +0000

    fix

--- js/script.js
+++ js/script.js
@@ -2,23 +2,20 @@
 //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
-// This script does basic math
+// This file contains the JS functions for index.html
 
 "use strict"
 
 function addAgeClicked () {
-  let length
-  let width
-  let area
-  let perimeter
+  // this function does area and perimeter
 
   // input
-  length = parseInt(document.getElementById("length-of-rectangle").value)
-  width = parseInt(document.getElementById("width-of-rectangle").value)
+  const length = parseInt(document.getElementById("length-of-rectangle").value)
+  const width = parseInt(document.getElementById("width-of-rectangle").value)
 
   // process
-  area = length * width
-  perimeter = 2 * (length + width)
+  const area = length * width
+  const perimeter = 2 * (length + width)
 
   // output
   document.getElementById('area').innerHTML = 'Area is: ' + length + ' x ' + width + ' = ' + area + ' or ' + area + ' cm²'
