03d0bf406f4812384e5121b6324b1edbfdc70144
Patrick Coxall <mr.coxall@mths.ca>
2021-05-03 00:54:09 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ICS2O-Unit2-04-HTML-Area_and_Perimeter
+[![GitHub's Super Linter](https://github.com/Mr-Coxall/ICS2O-Unit2-04-HTML-Area_and_Perimeter/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Mr-Coxall/ICS2O-Unit2-04-HTML-Area_and_Perimeter/actions)
+
+This website can be found at: [https://Mr-Coxall.github.io/ICS2O-Unit2-04-HTML-Area_and_Perimeter](https://Mr-Coxall.github.io/ICS2O-Unit2-04-HTML-Area_and_Perimeter)
---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This script does basic math
+
+function addAgeClicked () {
+  let length
+  let width
+  let area
+  let perimeter
+
+  // input
+  length = parseInt(document.getElementById("length-of-rectangle").value)
+  width = parseInt(document.getElementById("width-of-rectangle").value)
+
+  // process
+  area = length * width
+  perimeter = 2 * (length + width)
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + length + ' x ' + width + ' = ' + area + ' or ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: 2 x (' + length + ' + ' + width + ') = ' + perimeter + ' or ' + perimeter + ' cm'
+}
